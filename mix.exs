f83d868803ad7d7aa40af6c7d62765df80a71415
hqmq <michael@riesd.com>
2016-03-27 00:21:52 -0600

    add hackney to the project

--- mix.exs
+++ mix.exs
@@ -14,12 +14,13 @@ defmodule Homebody.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :nerves_ssdp_server, :nerves_ssdp_client, :thermex],
+    [applications: [:logger, :hackney, :nerves_ssdp_server, :nerves_ssdp_client, :thermex],
      mod: {Homebody, []}]
   end
 
   defp deps do
     [
+      {:hackney, "~> 1.6"},
       {:thermex, "~> 0.0.1"},
       {:nerves_ssdp_server, git: "https://github.com/nerves-project/nerves_ssdp_server", tag: "v0.2.0"},
       {:nerves_ssdp_client, git: "https://github.com/nerves-project/nerves_ssdp_client", branch: "master"},
